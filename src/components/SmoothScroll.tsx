c8b7f28f596de9d7d42f41246d07c1ddcc6f0cd9
Azanth S <sazanth75@gmail.com>
2026-06-02 17:11:55 +0530

    Fix GSAP SSR issue for production build

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -4,10 +4,10 @@ import Lenis from 'lenis';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-gsap.registerPlugin(ScrollTrigger);
-
 export default function SmoothScroll({ children }: { children: ReactNode }) {
   useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    
     const lenis = new Lenis({
       duration: 1.8,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
